698d58fb4521e20915fe55e3731e7e5014cf8f22
Edon <edon@valdman.works>
2024-04-27 17:08:52 -0500

    Added basic working version.

---
+++ Sources/KeyInputListener/KeyInputEvent.swift
@@ -0,0 +1,25 @@
+//
+//  KeyInputEvent.swift
+//  
+//
+//  Created by Edon Valdman on 4/27/24.
+//
+
+import Foundation
+
+public enum KeyInputEvent {
+    case character(Character)
+    case backspace
+    case space
+    case enter
+    case tab, backtab
+    case escape
+    
+    case specialKey(SpecialKey)
+    
+    #if canImport(AppKit)
+    public typealias SpecialKey = NSEvent.SpecialKey
+    #else
+    public typealias SpecialKey = Never
+    #endif
+}
--- Sources/KeyInputListener/KeyInputListener.swift
+++ Sources/KeyInputListener/KeyInputListener.swift
@@ -1,2 +1,139 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+//
+//  KeyInputListener.swift
+//
+//
+//  Created by Edon Valdman on 4/27/24.
+//
+
+import SwiftUI
+
+#if canImport(UIKit)
+typealias CrossPlatView = UIView
+#else
+typealias CrossPlatView = NSView
+#endif
+
+class _TextInput: CrossPlatView {
+    var keyboardConfig: KeyInputConfig = .init()
+    var keyInputEventHandler: ((KeyInputEvent) -> Void)? = nil
+    
+    private func newCharacter(_ char: Character) {
+        if char == " " {
+            keyInputEventHandler?(.space)
+        } else {
+            #if canImport(AppKit)
+            var modifiers = modifiers
+            if char.isUppercase && modifiers.contains(.shift) {
+                modifiers.subtract(.shift)
+            }
+            #endif
+            keyInputEventHandler?(.character(char))
+        }
+    }
+    
+    private func backspace() {
+        keyInputEventHandler?(.backspace)
+    }
+}
+
+#if canImport(UIKit)
+extension _TextInput: UIKeyInput {
+    // MARK: - UIKeyInput
+    
+    override var canBecomeFirstResponder: Bool { true }
+    
+    func insertText(_ text: String) {
+        for char in text {
+            if char.isNewline {
+                keyInputEventHandler?(.enter)
+            } else {
+                newCharacter(char)
+            }
+        }
+    }
+    
+    func deleteBackward() {
+        backspace()
+    }
+}
+#else
+import Carbon.HIToolbox
+extension _TextInput {
+    override var acceptsFirstResponder: Bool { true }
+    
+    override func keyDown(with keyInputEvent: NSEvent) {
+        // If pressed `escape`
+        if Int(keyInputEvent.keyCode) == kVK_Escape {
+            keyInputEventHandler?(.escape)
+        } else if let specialKey = keyInputEvent.specialKey {
+            // if pressed a special key
+            switch specialKey {
+            case .backspace, .delete:
+                backspace()
+            case .enter, .carriageReturn:
+                keyInputEventHandler?(.enter)
+            case .tab:
+                keyInputEventHandler?(.tab)
+            case .backTab:
+                keyInputEventHandler?(.backtab)
+            default:
+                keyInputEventHandler?(.specialKey(specialKey))
+            }
+        } else if let char = keyInputEvent.charactersIgnoringModifiers {
+            // otherwise, new character
+            newCharacter(Character(char))
+        }
+    }
+}
+#endif
+
+internal struct KeyInputListener {
+    internal typealias ViewType = _TextInput
+    
+    @Binding private var isFirstResponder: Bool
+    private let keyInputEventHandler: (KeyInputEvent) -> Void
+    
+    internal init(
+        isFirstResponder: Binding<Bool>,
+        keyInputEventHandler: @escaping (KeyInputEvent) -> Void
+    ) {
+        self._isFirstResponder = isFirstResponder
+        self.keyInputEventHandler = keyInputEventHandler
+    }
+    
+    private func createView() -> ViewType {
+        let view = ViewType()
+        view.keyInputEventHandler = self.keyInputEventHandler
+        return view
+    }
+}
+
+#if canImport(UIKit)
+extension KeyInputListener: UIViewRepresentable {
+    func makeUIView(context: Context) -> ViewType {
+        createView()
+    }
+    
+    func updateUIView(_ uiView: ViewType, context: Context) {
+        if isFirstResponder {
+            uiView.becomeFirstResponder()
+        } else {
+            uiView.resignFirstResponder()
+        }
+    }
+}
+#else
+extension KeyInputListener: NSViewRepresentable {
+    func makeNSView(context: Context) -> ViewType {
+        createView()
+    }
+    
+    func updateNSView(_ nsView: ViewType, context: Context) {
+        if isFirstResponder {
+            nsView.becomeFirstResponder()
+        } else {
+            nsView.resignFirstResponder()
+        }
+    }
+}
+#endif
